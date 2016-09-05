2d9d4d08f05de47a1516cab0a836413563daf208
spectator <spectatornan@gmail.com>
2016-09-05 17:39:12 +0800

    tacking

---
+++ runtimeDemo/UIViewController+Tracking.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Tracking.h
+//  runtimeDemo
+//
+//  Created by Spectator on 16/9/5.
+//  Copyright © 2016年 Spectator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Tracking)
+
+@end
---
+++ runtimeDemo/UIViewController+Tracking.m
@@ -0,0 +1,47 @@
+//
+//  UIViewController+Tracking.m
+//  runtimeDemo
+//
+//  Created by Spectator on 16/9/5.
+//  Copyright © 2016年 Spectator. All rights reserved.
+//
+
+#import "UIViewController+Tracking.h"
+
+@implementation UIViewController (Tracking)
+
+
++ (void)load {
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(viewWillAppear:);
+        SEL swizzledSelector = @selector(sn_viewWillAppear:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+        
+    });
+}
+
+
+#pragma mark -- Method Swizzling
+- (void)sn_viewWillAppear:(BOOL)animated {
+    
+//   在swizzling的过程中，方法中的[self xxx_viewWillAppear:animated]已经被重新指定到UIViewController类的-viewWillAppear:中。在这种情况下，不会产生无限循环。不过如果我们调用的是[self viewWillAppear:animated]，则会产生无限循环，因为这个方法的实现在运行时已经被重新指定为xxx_viewWillAppear:了。
+    [self sn_viewWillAppear:animated];
+    NSLog(@"viewWillAppear: %@",self);
+}
+
+@end
